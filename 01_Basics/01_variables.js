7dee8ec2fcfbf15bb9a4c1fede3832bd74125706
Adarsh <162902005+18ADP@users.noreply.github.com>
2025-02-13 17:02:54 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accId = 1234
+let email = "adarshpujari18@gmail.com"
+var acc = "ahsgf"
+address = "Bhubaneswar"
+let accountState;
+
+// accId = 2 // not allowed
+
+
+email = "hc@hc.com"
+acc = "21212121"
+address = "Bengaluru"
+
+console.log(accId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accId, email, acc, address, accountState]) 
