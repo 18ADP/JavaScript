61ad67f3e6f07f6b4b8ed12b98ec61413d0f4caf
Adarsh <162902005+18ADP@users.noreply.github.com>
2025-03-03 15:05:20 +0000

    controlFlow

---
+++ 04_Control_flow/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const m=5
+
+switch (m) {
+    case 1:
+        console.log("1")
+        break;
+    case 2:
+        console.log("2")
+        break;
+    case 3:
+        console.log("3")
+        break;
+    case 4:
+        console.log("4")
+        break;
+    case 5:
+        console.log("5 yohuuu")
+        break;
+
+    default:
+        console.log("ntg")
+        break;
+}
